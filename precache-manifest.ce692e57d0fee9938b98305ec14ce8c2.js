47f293e6584f31c955c3bb8b376d25f82bfa282e
bkwhello <30587026+bkwhello@users.noreply.github.com>
2019-10-19 11:13:13 +0200

    Updates

---
+++ precache-manifest.ce692e57d0fee9938b98305ec14ce8c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "016ebd31f7eea23595528af90de5fd3a",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "fb63b55106b9af3450d0",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "5669c55dfd5d871fdb59",
+    "url": "/robofriends/static/css/main.885d67ba.chunk.css"
+  },
+  {
+    "revision": "fb63b55106b9af3450d0",
+    "url": "/robofriends/static/js/2.c23c0b20.chunk.js"
+  },
+  {
+    "revision": "5669c55dfd5d871fdb59",
+    "url": "/robofriends/static/js/main.f2a74b04.chunk.js"
+  },
+  {
+    "revision": "8e199c7f211d8f157d3a",
+    "url": "/robofriends/static/js/runtime~main.a9498424.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
